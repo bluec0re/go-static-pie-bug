95a2e2d2e81f9cd38cf86c183ff0b200fdb33e9e
bluec0re <info@bluec0re.eu>
2023-08-21 09:09:02 +0200

    simplified

--- mod1.13/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/bluec0re/go-static-pie-bug
-
-go 1.13
--- test.sh
+++ test.sh
@@ -1,43 +1,48 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-export PATH="$HOME/sdk/go1.17/bin:/usr/bin:/bin"
+export PATH="/usr/bin:/bin"
 
 GO=$(command -v go)
 
 export CGO_ENABLED=0
 FLAGS=('-tags' 'netgo,osusergo,static_build' '-buildmode=pie' '-trimpath' '-ldflags' '-extldflags -static-pie -extld gcc -linkmode external')
 
-echo mod
+$GO version
+$GO env
+
+rm -f */foo */go-static-pie-bug
+
+echo -e '\x1b[94m# mod\x1b[m'
 $GO clean -cache -modcache
 (
   cd mod
+  echo -e " \x1b[94m# build\x1b[m"
   env -i GOOS=linux GOARCH=amd64 PATH=$PATH HOME=$HOME $GO build -mod=vendor "${FLAGS[@]}"
+  echo -e " \x1b[94m# build -o foo.go main.go\x1b[m"
   env -i GOOS=linux GOARCH=amd64 PATH=$PATH HOME=$HOME $GO build -mod=vendor "${FLAGS[@]}" -o foo main.go
 )
 
-echo mod1.13
-$GO clean -cache -modcache
-(
-  cd mod1.13
-  env -i GOOS=linux GOARCH=amd64 PATH=$PATH HOME=$HOME $GO build -mod=vendor "${FLAGS[@]}"
-  env -i GOOS=linux GOARCH=amd64 PATH=$PATH HOME=$HOME $GO build -mod=vendor "${FLAGS[@]}" -o foo main.go
-)
-
-echo path
+echo -e '\x1b[94m# path\x1b[m'
 $GO clean -cache -modcache
 (
   cd path
+  echo -e " \x1b[94m# build\x1b[m"
   env -i PATH=$PATH HOME=$HOME GO111MODULE=off GOPATH=$PWD $GO build "${FLAGS[@]}" github.com/bluec0re/go-static-pie-bug
+  echo -e " \x1b[94m# build -o foo.go main.go\x1b[m"
   env -i PATH=$PATH HOME=$HOME GO111MODULE=off GOPATH=$PWD $GO build "${FLAGS[@]}" -o foo src/github.com/bluec0re/go-static-pie-bug/main.go
 )
 
+echo -e '\x1b[94m# file\x1b[m'
 file */go-static-pie-bug */foo
+echo -e '\x1b[94m# ldd\x1b[m'
 ldd */go-static-pie-bug */foo
+echo -e '\x1b[94m# deadcode\x1b[m'
 grep --color -r 'This is not used' mod path
 
+echo -e '\x1b[94m# exec\x1b[m'
 set +e
 for b in ./*/go-static-pie-bug ./*/foo; do
-  echo $b
+  echo -e "\x1b[95m$b\x1b[m"
   $b
 done
